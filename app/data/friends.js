f22ed80ee2718a68c1cc22555ec22e06f41ed94e
Joel Gaeta <joelsgaeta@gmail.com>
2019-05-22 22:09:51 -0400

    fixed

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,30 +3,30 @@ var friends = [
     name: "Bryan Hadaway",
     photo:
       "https://i.pinimg.com/originals/64/91/3d/64913dd80f965ab83fb21cc170623ffe.png",
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    scores: [5, 4, 3, 3, 4, 1, 1, 4, 5, 5]
   },
   {
     name: "Harrison Kansol",
     photo:
       "https://cdn.bulbagarden.net/upload/thumb/0/02/129Magikarp.png/250px-129Magikarp.png",
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    scores: [5, 5, 5, 4, 2, 1, 3, 4, 5, 5]
   },
   {
     name: "Garrett Shea",
     photo:
       "https://www.vineyardvines.com/on/demandware.static/-/Sites-Vineyard-Vines-Library/default/dw89695fd1/images/logos/logo@2.6x.png",
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    scores: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
   },
   {
     name: "Riley Mann",
     photo:
       "https://www.bostonmagazine.com/wp-content/uploads/sites/2/2012/06/1191966371.jpg",
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    scores: [5, 3, 3, 3, 2, 2, 2, 1, 1, 1]
   },
   {
     name: "Jimmy Hadaway",
     photo: "https://www.organicfacts.net/wp-content/uploads/herbsinfo.jpg",
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    scores: [5, 4, 5, 2, 3, 1, 5, 5, 4, 3]
   }
 ];
 
